e062661ac34f1a4c389888540f52a3eed7764faa
yrafzrar <rafarl.areis@outlook.com>
2026-02-05 07:34:21 -0300

    Vereificador de região
    Retorna a região de emissão de cpf

--- validadorCPF.py
+++ validadorCPF.py
@@ -1,9 +1,26 @@
 import string
+def retorna_regiao(cpf):
+    regioes = {
+        '1': 'DF, GO, MT, MS, TO',
+        '2': 'AC, AM, AP, PA, RO, RR',
+        '3': 'CE, MA, PI',
+        '4': 'PB, PE, AL, RN',
+        '5': 'BA, SE',
+        '6': 'MG',
+        '7': 'ES, RJ',
+        '8': 'SP',
+        '9': 'PR, SC',
+        '0': 'RS'
+    }
+    return regioes.get(cpf[8])
+
 remocao_de_simbolos = str.maketrans("", "", string.punctuation)
 cpf = input("Insira o CPF a ser válidado: \n>").translate(remocao_de_simbolos)
 def invalido(x):
     print(f"CPF inválido, {x} digito não corresponde")
-
+def regiao(x, y):
+    if cpf[10] == f'{x}':
+        print(f"CPF gerado na região do {y}")
 nove_digitos = cpf[:9]
 if nove_digitos.isnumeric() and len(cpf) == 11:
     contagem10dig = 10
@@ -28,6 +45,11 @@ if nove_digitos.isnumeric() and len(cpf) == 11:
         digito11 = 0 if resultado_11digito >= 9 else resultado_11digito
         if digito11 == int(cpf[10]):
             print("CPF válido")
+
+            infos = input("Deseja mais informações?\n[s]im [n]ão\n>").strip().lower()
+            if infos == 's':
+                print(f"CPF gerado na região de {retorna_regiao(cpf)}")
+                
         else:
             invalido(11)
     else:
